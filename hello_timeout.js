4a5ac9f8d2a3ca1257b47013f550097e8f6899c5
colspen <spencerlewiscole@gmail.com>
2022-09-27 14:04:10 +0000

    changed git folder

---
+++ hello_timeout.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+  process.stdout.write("43llo ");
+}, 1100);
+setTimeout(() => {
+  process.stdout.write("th3r3");
+}, 1600);
+setTimeout(() => {
+  process.stdout.write(", ");
+}, 1900);
+setTimeout(() => {
+  process.stdout.write("w0rld.");
+}, 3500);
+setTimeout(() => {
+  console.log()
+}, 5000);
+
+
+
---
+++ typewriter.js
@@ -0,0 +1,36 @@
+let str = "At last..... I have finally learned to communicate with you directly through the command line. \n";
+
+
+const interval = setInterval(() => {
+
+  process.stdout.write(str[0]);
+  str = str.slice(1);
+  
+  if (!str.length) {
+    clearInterval(interval);
+  }
+}, 75);
+
+
+// setTimeout(() => {
+//   console.log();
+// }, 1500);
+
+// let i = 0;
+// const writer = () => {
+//   process.stdout.write(str[i]);
+//   i++;
+
+ 
+//   if (i >= str.length) {
+//     setTimeout(() => {
+//       clearInterval(delay);
+//       console.log()
+//     }, 450);
+//   }
+
+  
+// };
+
+// let delay = setInterval(writer, 65);
+
